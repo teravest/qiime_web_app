ae9831870daee7dd5a8b8bf0f61a1b0f4c5ab34f
teravest <ejteravest@gmail.com>
2014-03-07 14:59:18 -0700

    forgot one

--- user_scripts/DonationProcessing/bin_by_swab_count.py
+++ user_scripts/DonationProcessing/bin_by_swab_count.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
+
+__author__ = "Daniel McDonald"
+__copyright__ = "Copyright 2009-2013, QIIME Web Analysis"
+__credits__ = ["Daniel McDonald"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = ["Daniel McDonald"]
+__email__ = "mcdonadt@colorado.edu"
+__status__ = "Development"
+
 from sys import argv
 
 def write_stuff(header, data, fname):
     f = open(fname, 'w')
     f.write('\t'.join(header))
     f.write('\n')
-    f.write('\n'.join(['\t'.join(i) for i in buf]))
+    f.write('\n'.join(['\t'.join(i) for i in data]))
     f.write('\n')
     f.close()
 
